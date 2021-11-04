9f50717f55d21a03114be252899adcef76cb887b
romaintailhurat <romain.tailhurat@gmail.com>
2021-11-04 11:11:08 +0000

    SEP pipeline, french census standardization

--- code/Python/sep.py
+++ code/Python/sep.py
@@ -9,38 +9,68 @@ from zipfile import ZipFile
 from io import BytesIO
 from datetime import timedelta
 import pandas as pd
+import numpy as np
 
 # Fns ----
 
-def raw_french_to_standard(df, age_classes):
-    v = df["AGED100"]
+def raw_french_to_standard(df, age_classes, nuts3):
+    """Transform the french census data source to our standard format.
+
+    Arguments:
+        df {dataframe} -- data source
+        age_classes {list} -- a vector holding the age classes
+    """
+    # Adding a age class column
+    ages_vector = df["AGED100"]
     *age_classes_not_100, _ = age_classes
-    c = pd.cut(v, 20, labels=age_classes_not_100, right=False)
-    df["AGE_CLASS"] = c
+    age_groups = pd.cut(ages_vector, 20, labels=age_classes_not_100, right=False)
+    df["AGE_CLASS"] = age_groups
     df["AGE_CLASS"] = df["AGE_CLASS"].cat.add_categories(["Y_GE100"])
-    print(df["AGE_CLASS"].cat.categories)
-    df.loc[df["AGED100"] == 100, "AGE_CLASS"] = "Y_GE100" 
-    print(df)
+    df.loc[df["AGED100"] == 100, "AGE_CLASS"] = "Y_GE100"
+
+    # Grouping by age class
+    df = df.groupby(by=["CODGEO", "SEXE","AGE_CLASS"])["NB"].sum().reset_index()
+
+    # Adding NUTS3
+    df["CODGEO_COURT"] = df.apply(lambda row: row.CODGEO[0:3] if row.CODGEO[0:2] == "97" else row.CODGEO[0:2], axis=1) # FIXME column-wise
+
+    df_with_nuts = df.merge(nuts3, left_on="CODGEO_COURT", right_on="departement")
+
+    df_selected_cols = df_with_nuts[["CODGEO", "nuts3", "SEXE", "AGE_CLASS", "NB"]]
+
+    df_final = df_selected_cols.rename(columns={"CODGEO": "lau", "SEXE": "sex", "AGE_CLASS": "age", "NB": "population"})
+
+    print(df_final)
+
+    return(df_final)
 
 def raw_italian_to_standard(df):
     pass
 
 # Tasks ----
 
-@task(cache_for=timedelta(hours=1))
+@task
 def get_age_class_data():
     resp = get("https://raw.githubusercontent.com/INTERSTAT/Statistics-Contextualized/main/age-groups.csv")
     data = BytesIO(resp.content)
     df = pd.read_csv(data)
     return(list(df["group"]))
 
-@task(cache_for=timedelta(hours=1))
-def extract_french_census(url, age_classes):
+@task
+def get_nuts3():
+    resp = get("https://raw.githubusercontent.com/INTERSTAT/Statistics-Contextualized/main/nuts3.csv")
+    data = BytesIO(resp.content)
+    df = pd.read_csv(data)
+    return(df)
+
+@task
+def extract_french_census(url, age_classes, nuts3):
     resp = get(url)
     zip = ZipFile(BytesIO(resp.content))
     file_in_zip = zip.namelist().pop()
     df = pd.read_csv(zip.open(file_in_zip), sep=";")
-    raw_french_to_standard(df, age_classes)
+    df["CODGEO"] = df["CODGEO"].astype("string")
+    raw_french_to_standard(df, age_classes, nuts3)
     return(df)
 
 @task
@@ -51,10 +81,12 @@ def extract_italian_census(url):
     return(df)
 
 with Flow("sep") as flow:
-    age_classes = default=get_age_class_data()
+    age_classes = get_age_class_data()
+
+    nuts3 = get_nuts3()
 
     french_census_data_url = Parameter("fr_url", default="https://www.insee.fr/fr/statistiques/fichier/5395878/BTT_TD_POP1B_2018.zip")
-    french_census = extract_french_census(french_census_data_url, age_classes)
+    french_census = extract_french_census(french_census_data_url, age_classes, nuts3)
 
     italian_census_data_url = Parameter("it_url", default="https://minio.lab.sspcloud.fr/l4tu7k/Census_IT_2018.csv")
     extract_italian_census(italian_census_data_url)
