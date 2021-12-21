91dd4930cb0185a5b91e4be276df67b0058949c6
ylzbwc <thomas.dubois@insee.fr>
2021-12-21 09:32:56 +0000

    add list and intended types of variables as Parameter

--- code/Python/gf/bpe-extraction.py
+++ code/Python/gf/bpe-extraction.py
@@ -6,27 +6,20 @@ from io import BytesIO
 
 # Constants
 PUSH_TO_PREFECT_CLOUD_DASHBOARD = False
-# List and intended types of selected variables
-types = {"AN": str, "COUVERT": str, "DEPCOM": str, "ECLAIRE": str, "LAMBERT_X": float, "LAMBERT_Y": float, "NBSALLES": int, "QUALITE_XY": str, "TYPEQU": str}
-
 
 @task
-def extract_french_all(url):
-
+def extract_french_data(url, types): # types = List and intended types of selected variables
     archive = ZipFile(BytesIO(requests.get(url).content))
-
-    metadataZip = [name for name in archive.namelist() if name.startswith("varmod")][0]
-    dataZip = [name for name in archive.namelist() if not name.startswith("varmod")][0]
-
-    bpeFrame = pd.read_csv(archive.open(dataZip), sep=";", usecols=types.keys()) # 'dtype = types' not used here because of 'NA' values
-
-    return bpeFrame
+    data_zip = [name for name in archive.namelist() if not name.startswith("varmod")][0]
+    bpe_data = pd.read_csv(archive.open(data_zip), sep=";", usecols=types.keys()) # 'dtype = types' not used here because of 'NA' values
+    return bpe_data
 
 # Build flow
 def build_flow():
     with Flow("GF-EF") as flow:
         bpe_zip_url = Parameter(name="bpe_zip_url", required=True)
-        french_data = extract_french_all(bpe_zip_url)
+        types = Parameter(name="types", required = False)
+        french_data = extract_french_data(bpe_zip_url, types)
     return flow
 
 # Run flow
@@ -36,8 +29,8 @@ if __name__ == "__main__":
         flow.register(project_name="sample")
     else:
         flow.run(parameters={
-            "bpe_zip_url": "https://www.insee.fr/fr/statistiques/fichier/3568638/bpe20_sport_Loisir_xy_csv.zip"})
-    flow.visualize()
+            "bpe_zip_url": "https://www.insee.fr/fr/statistiques/fichier/3568638/bpe20_sport_Loisir_xy_csv.zip", "types": {"AN": str, "COUVERT": str, "DEPCOM": str, "ECLAIRE": str, "LAMBERT_X": float, "LAMBERT_Y": float, "NBSALLES": int, "QUALITE_XY": str, "TYPEQU": str}})
+    # flow.visualize()
 
 
 
