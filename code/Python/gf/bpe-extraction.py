a92d82a5dd288c6311dd43871ee8fcf97e51d339
ylzbwc <thomas.dubois@insee.fr>
2021-12-21 14:48:24 +0000

    Add provisonal connection to FTP. Waiting for management of credentials

--- code/Python/gf/bpe-extraction.py
+++ code/Python/gf/bpe-extraction.py
@@ -3,9 +3,13 @@ import pandas as pd
 from prefect import task, Flow, Parameter
 from zipfile import ZipFile
 from io import BytesIO
+import pysftp
 
 # Constants
 PUSH_TO_PREFECT_CLOUD_DASHBOARD = False
+FTP_HOST = 'FTP_HOST'
+FTP_USERNAME = 'FTP_USERNAME'
+FTP_PASSWORD = 'FTP_PASSWORD'
 
 @task
 def extract_french_data(url, types={}, facilities_filter={}): # types = List and intended types of selected variables. facilities_filter = list of type of facilities selected
@@ -22,6 +26,17 @@ def extract_french_data(url, types={}, facilities_filter={}): # types = List and
         return bpe_data_filtered
     return bpe_data
 
+@task 
+def write_csv_on_ftp(df):
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None  
+    # TODO: Use tempfile
+    csv = df.to_csv (r'gf_data_fr.csv', index = False, header=True)
+    # Commented lines while waiting for details on the FTP connection
+    # with pysftp.Connection(FTP_HOST, username=FTP_USERNAME, password=FTP_PASSWORD, cnopts=cnopts) as sftp:
+        # with sftp.cd('files/gf/output/'):
+             # sftp.put('gf_data_fr.csv')
+
 # Build flow
 def build_flow():
     with Flow("GF-EF") as flow:
@@ -29,6 +44,7 @@ def build_flow():
         types = Parameter(name="types", required = False)
         facilities_filter = Parameter(name="facilities_filter", required=False)
         french_data = extract_french_data(bpe_zip_url, types, facilities_filter)
+        write_csv_on_ftp(french_data)
     return flow
 
 # Run flow
@@ -40,7 +56,7 @@ if __name__ == "__main__":
         # run without facilities_filter
         flow.run(parameters={"bpe_zip_url": "https://www.insee.fr/fr/statistiques/fichier/3568638/bpe20_sport_Loisir_xy_csv.zip",
              "types": {"AN": str, "COUVERT": str, "DEPCOM": str, "ECLAIRE": str, "LAMBERT_X": float, "LAMBERT_Y": float, "NBSALLES": int, "QUALITE_XY": str, "TYPEQU": str}})
-    flow.visualize()
+    # flow.visualize()
 
 
 
