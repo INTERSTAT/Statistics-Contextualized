ac87f2fd1da11d8bfb01e71d79d9cffb45c70e12
ylzbwc <thomas.dubois@insee.fr>
2021-12-21 14:14:57 +0000

    add optional parameter for filtering by type of facilities

--- code/Python/gf/bpe-extraction.py
+++ code/Python/gf/bpe-extraction.py
@@ -8,14 +8,18 @@ from io import BytesIO
 PUSH_TO_PREFECT_CLOUD_DASHBOARD = False
 
 @task
-def extract_french_data(url, types={}): # types = List and intended types of selected variables
+def extract_french_data(url, types={}, facilities_filter={}): # types = List and intended types of selected variables. facilities_filter = list of type of facilities selected
     archive = ZipFile(BytesIO(requests.get(url).content))
     data_zip = [name for name in archive.namelist() if not name.startswith("varmod")][0]
     # if types is not specified, take all variables
     if not types:
         bpe_data = pd.read_csv(archive.open(data_zip), sep=";") # 'dtype = types' not used here because of 'NA' values
     else:
-        bpe_data = pd.read_csv(archive.open(data_zip), sep=";", usecols=types.keys()) # 'dtype = types' not used here because of 'NA' values
+        bpe_data = pd.read_csv(archive.open(data_zip), sep=";", usecols=types.keys())
+    # if facilities _filter is not empty, select only type of facilities starting with list of facility types 
+    if facilities_filter:
+        bpe_data_filtered = bpe_data.loc[(bpe_data["TYPEQU"].str.startswith(tuple(facilities_filter)))]
+        return bpe_data_filtered
     return bpe_data
 
 # Build flow
@@ -23,7 +27,8 @@ def build_flow():
     with Flow("GF-EF") as flow:
         bpe_zip_url = Parameter(name="bpe_zip_url", required=True)
         types = Parameter(name="types", required = False)
-        french_data = extract_french_data(bpe_zip_url, types)
+        facilities_filter = Parameter(name="facilities_filter", required=False)
+        french_data = extract_french_data(bpe_zip_url, types, facilities_filter)
     return flow
 
 # Run flow
@@ -32,6 +37,7 @@ if __name__ == "__main__":
     if PUSH_TO_PREFECT_CLOUD_DASHBOARD:
         flow.register(project_name="sample")
     else:
+        # run without facilities_filter
         flow.run(parameters={"bpe_zip_url": "https://www.insee.fr/fr/statistiques/fichier/3568638/bpe20_sport_Loisir_xy_csv.zip",
              "types": {"AN": str, "COUVERT": str, "DEPCOM": str, "ECLAIRE": str, "LAMBERT_X": float, "LAMBERT_Y": float, "NBSALLES": int, "QUALITE_XY": str, "TYPEQU": str}})
     flow.visualize()
