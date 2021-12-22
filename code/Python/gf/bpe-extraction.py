487179098119286ea24c8f28e9b9c343fec06cde
Thomas Dubois <thomas.dubois@insee.fr>
2021-12-22 16:24:28 +0100

    Rewrite extract data and init extract metadata (#22)
    * Separation between build and run flow
    
    * add list and intended types of variables as Parameter
    
    * add list of variables as optional
    
    * delete useless print
    
    * add optional parameter for filtering by type of facilities
    
    * Add provisional connection to FTP. Waiting for management of credentials
    
    * Move facilities filter as tuple instead of dictionary
    
    * adding get metadata and get list of facility types

--- code/Python/gf/bpe-extraction.py
+++ code/Python/gf/bpe-extraction.py
@@ -3,29 +3,86 @@ import pandas as pd
 from prefect import task, Flow, Parameter
 from zipfile import ZipFile
 from io import BytesIO
+import pysftp
 
-# List and intended types of selected variables
-types = {"AN": str, "COUVERT": str, "DEPCOM": str, "ECLAIRE": str, "LAMBERT_X": float, "LAMBERT_Y": float, "NBSALLES": int, "QUALITE_XY": str, "TYPEQU": str}
+# Constants
+PUSH_TO_PREFECT_CLOUD_DASHBOARD = False
+FTP_HOST = 'FTP_HOST'
+FTP_USERNAME = 'FTP_USERNAME'
+FTP_PASSWORD = 'FTP_PASSWORD'
+					
+types_var_mod = {"COD_VAR": str, "LIB_VAR": str, "COD_MOD": str, "LIB_MOD": str, "TYPE_VAR": str, "LONG_VAR": int}
 
 @task
-def extract_french_all(url):
-
+def extract_french_data(url, types={}, facilities_filter=()): # types = List and intended types of selected variables. facilities_filter = list of type of facilities selected
     archive = ZipFile(BytesIO(requests.get(url).content))
+    data_zip = [name for name in archive.namelist() if not name.startswith("varmod")][0]
+    # if types is not specified, take all variables
+    if not types:
+        bpe_data = pd.read_csv(archive.open(data_zip), sep=";") # 'dtype = types' not used here because of 'NA' values
+    else:
+        bpe_data = pd.read_csv(archive.open(data_zip), sep=";", usecols=types.keys())
+    # if facilities _filter is not empty, select only type of facilities starting with list of facility types 
+    if facilities_filter:
+        bpe_data_filtered = bpe_data.loc[(bpe_data["TYPEQU"].str.startswith(facilities_filter))]
+        return bpe_data_filtered
+    return bpe_data
 
-    metadataZip = [name for name in archive.namelist() if name.startswith("varmod")][0]
-    dataZip = [name for name in archive.namelist() if not name.startswith("varmod")][0]
 
-    bpeFrame = pd.read_csv(archive.open(dataZip), sep=";", usecols=types.keys()) # 'dtype = types' not used here because of 'NA' values
+@task
+def extract_french_metadata(url, types={}):
+    archive = ZipFile(BytesIO(requests.get(url).content))
+    metadata_zip = [name for name in archive.namelist() if name.startswith("varmod")][0]
+    bpe_metadata = pd.read_csv(archive.open(metadata_zip), sep=";", usecols=types_var_mod.keys())
+    if types:
+            bpe_metadata_filtered = bpe_metadata.loc[bpe_metadata["COD_VAR"].isin(types)]
+            return bpe_metadata_filtered
+    return bpe_metadata
 
-    return bpeFrame
+
+@task
+def get_typ_equ(bpe_metadata):
+    typ_equ = bpe_metadata.loc[(bpe_metadata["COD_VAR"] == "TYPEQU"), ["COD_MOD", "LIB_MOD"]]
+    return typ_equ
 
 
-with Flow("GF-EF") as flow:
+@task 
+def write_csv_on_ftp(df):
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None  
+    # TODO: Use tempfile
+    csv = df.to_csv (r'gf_data_fr.csv', index = False, header=True)
+    # Commented lines while waiting for details on the FTP connection
+    # with pysftp.Connection(FTP_HOST, username=FTP_USERNAME, password=FTP_PASSWORD, cnopts=cnopts) as sftp:
+        # with sftp.cd('files/gf/output/'):
+             # sftp.put('gf_data_fr.csv')
 
-    bpeZipURL = Parameter("bpeZipURL")
 
-    french_data = extract_french_all(bpeZipURL)
-   
-    flow.run(bpeZipURL = "https://www.insee.fr/fr/statistiques/fichier/3568638/bpe20_sport_Loisir_xy_csv.zip")
+# Build flow
+def build_flow():
+    with Flow("GF-EF") as flow:
+        bpe_zip_url = Parameter(name="bpe_zip_url", required=True)
+        types = Parameter(name="types", required = False)
+        facilities_filter = Parameter(name="facilities_filter", required=False)
+        french_data = extract_french_data(bpe_zip_url, types, facilities_filter)
+        write_csv_on_ftp(french_data)
+        french_metadata = extract_french_metadata(bpe_zip_url, types)
+        typ_equ = get_typ_equ(french_metadata)
+    return flow
 
-flow.visualize()
+ 
+# Run flow
+if __name__ == "__main__":
+    flow = build_flow()
+    if PUSH_TO_PREFECT_CLOUD_DASHBOARD:
+        flow.register(project_name="sample")
+    else:
+        # run without facilities_filter
+        flow.run(parameters={"bpe_zip_url": "https://www.insee.fr/fr/statistiques/fichier/3568638/bpe20_sport_Loisir_xy_csv.zip",
+             "types": {"AN": str, "COUVERT": str, "DEPCOM": str, "ECLAIRE": str, "LAMBERT_X": float, "LAMBERT_Y": float, "NBSALLES": int, "QUALITE_XY": str, "TYPEQU": str}})
+    flow.visualize()
+
+
+
+
+
