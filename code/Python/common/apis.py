1ffd343871e9566cac1fee34a035a2c7995b6ba7
Thomas Dubois <thomas.dubois@insee.fr>
2022-03-28 17:13:43 +0200

    fix: add filter for opened school

--- code/Python/common/apis.py
+++ code/Python/common/apis.py
@@ -13,9 +13,10 @@ def get_french_schools_data() -> pd.DataFrame:
     base_url = "https://data.education.gouv.fr/api/v2/catalog/datasets"
     dataset_id = "fr-en-adresse-et-geolocalisation-etablissements-premier-et-second-degre"
     row_limit = 5
+    filter_school_state = 1
     # TODO create two lists and join'em around 'AS' ?
     cols = ["numero_uai AS school_id", "appellation_officielle AS name", "latitude", "longitude", "code_commune AS lau", "secteur_public_prive_libe AS institution_type"]
     cols_request = quote(",".join(cols))
-    target = f"{base_url}/{dataset_id}/exports/csv?select={cols_request}&limit={str(row_limit)}&offset=0&timezone=UTC"
+    target = f"{base_url}/{dataset_id}/exports/csv?select={cols_request}&limit={str(row_limit)}&refine.etat_etablissement={filter_school_state}&offset=0&timezone=UTC"
     df = pd.read_csv(target)
     return df
